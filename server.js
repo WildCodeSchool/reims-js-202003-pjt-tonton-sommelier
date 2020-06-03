8e0ebf7dc1844ac4cd86e0b6b04b17766af6d7ca
jojo051 <youpi051@yahoo.fr>
2020-06-03 10:51:31 +0200

    build(server&test): create server and first test

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+
+app.listen(port, (err) => {
+  if (err) {
+    throw new Error('Something bad happened...');
+  }
+  console.log(`Server is listening on ${port}`);
+});
+ 
+app.get('/',(req, res) =>{
+  res.status(200).send('hello world et tonton');
+})
