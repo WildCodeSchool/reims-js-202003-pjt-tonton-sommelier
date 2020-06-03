a2c8b61e9d42e711862170959ee5c8af59a249c8
lucas <lucas-p52@hotmail.fr>
2020-06-03 14:21:30 +0200

    refactor(app): add a route, create a test and clean tonton_sommelier

--- server/server.js
+++ server/server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+const connection = require('./conf');
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.listen(port, (err) => {
   if (err) {
@@ -13,6 +18,16 @@ app.get('/',(req, res) =>{
   res.status(200).send('hello world et tonton');
 })
 
-app.post('/',(req, res) =>{
-  res.status(200).send('hello world et tonton');
-})
+app.post('/boxes', (req, res) => {
+
+  const formData = req.body;
+  connection.query('INSERT INTO box SET ?', formData, (err, results) => {
+
+    if (err) {
+      console.log(err);
+      res.status(500).send("Erreur lors de la sauvegarde d'une box");
+    } else {
+      res.sendStatus(200).send(res);
+    }
+  });
+});
