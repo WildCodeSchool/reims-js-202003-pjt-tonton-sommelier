7c97c44c4048f13abd096f79eac5c2401f02a1fa
brithe00 <bthellier51@gmail.com>
2020-06-10 16:07:35 +0200

    refactor(archivesql): created table description, and box_description

--- server/archive.sql
+++ server/archive.sql
@@ -1,3 +1,5 @@
+/* SET FOREIGN_KEY_CHECKS = 0; */
+
 CREATE DATABASE tonton_sommelier;
 
 USE tonton_sommelier;
@@ -7,3 +9,30 @@ CREATE TABLE `box`(
 	`name` VARCHAR (100) NOT NULL,
 	PRIMARY KEY (`id`)
 );
+
+------------------------------------------
+
+CREATE TABLE `description` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `content` VARCHAR(255),
+	`type` VARCHAR(50),
+    PRIMARY KEY (`id`)
+);
+
+/* Création de la table de jointure 'box_description' */
+CREATE TABLE `box_description` (
+	`box_id` INT(11) DEFAULT NULL,
+	`description_id` INT(11) DEFAULT NULL
+);
+
+/* Ajout des clefs étrangères de la table 'box_description' */
+
+ALTER TABLE box_description
+ADD CONSTRAINT `fk_box_description_box`
+FOREIGN KEY (`box_id`)
+REFERENCES `box`(`id`);
+
+ALTER TABLE box_description
+ADD CONSTRAINT `fk_box_description_description`
+FOREIGN KEY (`description_id`)
+REFERENCES `description`(`id`);
