4dd93cffd7686338ea02ef123b15e49276e7045d
brithe00 <bthellier51@gmail.com>
2020-06-10 16:08:18 +0200

    refactor(serverjs): created CRUD routes for description, and added cors

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
 const port = 8000;
-const connection = require('../conf');
+const cors = require('cors');
+const connection = require('./conf');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.listen(port, (err) => {
@@ -10,7 +11,8 @@ app.listen(port, (err) => {
   }
   console.log(`Server is listening on ${port}`);
 });
- 
+app.use(cors()); 
+
 app.get('/',(req, res) =>{
   res.status(200).send('hello tonton sommelier');
 });
@@ -53,4 +55,54 @@ app.post('/boxes', (req, res) => {
       }
     });
   }
+});
+
+/* ------------------------partie descriptions ------------------------*/
+app.get('/descriptions',(req, res) =>{
+  connection.query('SELECT * from description', (err, results) => {
+    if (err) {
+      res.status(500).send('Erreur lors de la récupération des descriptions');
+    } else {
+      res.status(200).json(results);
+    }
+  });
+});
+
+app.post('/descriptions', (req, res) => {
+  const formData = req.body;
+  if (formData.content == null || formData.content === '') {
+    res.status(400).send("La description est mal renseignée");
+  } else {
+    connection.query('INSERT INTO description SET ?', formData, (err, results) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send("Erreur lors de la sauvegarde de la description");
+      } else {
+        res.status(201).send({...formData, id:results.insertId });
+      }
+    });
+  }
+});
+
+app.put('/descriptions/:id', (req, res) => {
+  const idDescription = req.params.id;
+  const formData = req.body;
+  connection.query('UPDATE description SET ? WHERE id = ?', [formData, idDescription], err => {
+    if (err) {
+      console.log(err);
+      res.status(500).send("Erreur lors de la modification de la description");
+    } else {
+      res.status(200).send({...formData})} 
+    });
+});
+
+app.delete('/descriptions/:id', (req, res) => {
+  const idDescription = req.params.id;
+  connection.query('DELETE FROM description WHERE id = ?', [idDescription], err => {
+    if (err) {
+      res.status(500).send(`Erreur lors de la suppression de la description`);
+    } else {
+      res.status(200).send(`Élément supprimé avec succès`);
+    }
+  });
 });
