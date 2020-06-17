981c1a5c084dd31216045a2bc90ef5cfa7f1421c
lucas <lucas-p52@hotmail.fr>
2020-06-17 11:27:28 +0200

    refactor(integration): test show description

--- server/server.integration.spec.js
+++ server/server.integration.spec.js
@@ -2,6 +2,7 @@ const request = require('supertest');
 const app = require('./server.js');
 const connection = require('./db.js');
 
+/*---- test hello tonton sommelier -------*/
 
 describe('route test', () => {
   it('acces to hello tonton_sommelier', (done) => {
@@ -17,3 +18,19 @@ describe('route test', () => {
   });
 });
 
+/*----------------test description---------------------- */
+
+describe('route test', () => {
+  it('Show description succed', (done) => {
+    request(app)
+      .get('/descriptions')
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .then(response => {
+        const expected = expect.any(Array)
+        /* question pour romain : const expected = response.map(rep => expect.any({rep.id : expect.any(Number), rep.content : expect.any(String), rep.type : expect.any(String)}))*/
+        expect(response.body).toEqual(expected);
+        done();
+      });
+  });
+});
