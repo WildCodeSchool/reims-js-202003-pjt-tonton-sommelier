3bd954ea978061b43a47dde2587c5c308754c5d1
lucas <lucas-p52@hotmail.fr>
2020-07-07 16:12:12 +0200

    refactor(archive): add datas to get beter tests

--- server/archive.sql
+++ server/archive.sql
@@ -67,15 +67,15 @@ INSERT INTO category (`id`, `name`) VALUES (1, 'rouge'), (2, 'blanc') ;
 INSERT INTO box (`id`, `name`, `category_id`) VALUES (1,'Mystère', 1);
 
 INSERT INTO content (`id`, `content`, `choix`, `réponse`,`type`) VALUES 
-(1, 'lorem ipsum inglorious minous santanas', 1, false,'nez'),
-(2, 'lorem ipsum inglorious minous santanas', 2, false,'nez'),
-(3, 'lorem ipsum inglorious minous santanas', 3, true,'nez'),
-(4, 'lor222em ip222sum in222glorious mino222us sa222ntanas', 1, false,'oeil'),
-(5, 'lor222em ip222sum in222glorious mino222us sa222ntanas', 2, true, 'oeil'),
-(6, 'lor222em ip222sum in222glorious mino222us sa222ntanas', 3, false, 'oeil'),
-(7, 'question 1 : lor333em ips3333um inglo33333rious m3333inous sant33333anas', 1, false,'bouche'),
-(8, 'question 2 : lor333em ips3333um inglo33333rious m3333inous sant33333anas', 2, false,'bouche'),
-(9, 'question 3 :lor333em ips3333um inglo33333rious m3333inous sant33333anas', 3, true,'bouche'),
+(1, 'nezlorem ipsum inglorious minous santanas', 1, false,'nez'),
+(2, 'nezlorem ipsum inglorious minous santanas', 2, false,'nez'),
+(3, 'nezlorem ipsum inglorious minous santanas', 3, true,'nez'),
+(4, 'oeillor222em ip222sum in222glorious mino222us sa222ntanas', 1, false,'oeil'),
+(5, 'oeillor222em ip222sum in222glorious mino222us sa222ntanas', 2, true, 'oeil'),
+(6, 'oeillor222em ip222sum in222glorious mino222us sa222ntanas', 3, false, 'oeil'),
+(7, 'bouchequestion 1 : lor333em ips3333um inglo33333rious m3333inous sant33333anas', 1, false,'bouche'),
+(8, 'bouchequestion 2 : lor333em ips3333um inglo33333rious m3333inous sant33333anas', 2, false,'bouche'),
+(9, 'bouchequestion 3 :lor333em ips3333um inglo33333rious m3333inous sant33333anas', 3, true,'bouche'),
 (10, 'ceci nest pas bon', 1, true,'bouche');
 
 INSERT INTO category_content (`category_id`, `content_id`) VALUES (1,1),(1,2),(1,3),(1,4),(1,5),(1,6),(1,7),(1,8),(1,9),(2,10);
